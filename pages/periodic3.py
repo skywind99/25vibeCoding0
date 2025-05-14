cd3c23898c1d26c86bb1dc005125db4c0eeec5a7
skywind99 <skywind99@naver.com>
2025-05-14 19:34:27 +0900

    Create periodic3.py

---
+++ pages/periodic3.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import streamlit.components.v1 as components
+import os
+
+st.set_page_config(layout="wide")
+
+html_path = os.path.join(os.path.dirname(__file__), "periodic_simulator2.html")
+with open(html_path, "r", encoding="utf-8") as f:
+    html_code = f.read()
+
+components.html(html_code, height=800, width=0, scrolling=True)
