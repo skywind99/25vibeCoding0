2cea7514d4091253e13a37fa80f5df5f7f3269aa
skywind99 <skywind99@naver.com>
2025-05-14 11:41:42 +0900

    Update periodic2.py

--- pages/periodic2.py
+++ pages/periodic2.py
@@ -2,7 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 # 수정된 경로로 HTML 파일 읽기
-with open("/periodic_simulator_fixed.html", "r", encoding="utf-8") as f:
+with open("periodic_simulator_fixed.html", "r", encoding="utf-8") as f:
     html_code = f.read()
 
 # Streamlit에 HTML 렌더링
