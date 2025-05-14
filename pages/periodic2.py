e62cc6ca71c94f7a41210e7581ee04cdce7b103d
skywind99 <skywind99@naver.com>
2025-05-14 11:53:10 +0900

    Update periodic2.py

--- pages/periodic2.py
+++ pages/periodic2.py
@@ -1,11 +1,11 @@
+import streamlit as st
 import streamlit.components.v1 as components
 import os
 
-# 현재 파일 기준으로 HTML 경로 잡기
-current_dir = os.path.dirname(__file__)  # pages/
-html_path = os.path.join(current_dir, "periodic_simulator_fixed.html")
+st.set_page_config(layout="wide")
 
+html_path = os.path.join(os.path.dirname(__file__), "periodic_simulator_fixed.html")
 with open(html_path, "r", encoding="utf-8") as f:
     html_code = f.read()
 
-components.html(html_code, width=0, height=1000, scrolling=True)
+components.html(html_code, height=800, width=0, scrolling=True)
