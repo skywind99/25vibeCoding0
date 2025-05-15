563db7dd04a74f48738f3d86de21aeec613c82e2
skywind99 <skywind99@naver.com>
2025-05-15 22:40:36 +0900

    Update 20steps.py

--- pages/20steps.py
+++ pages/20steps.py
@@ -2,7 +2,8 @@ import streamlit as st
 import openai
 
 # API 키 불러오기
-openai.api_key = st.secrets["STREAMLIT_KEY"]
+
+api_key = os.getenv("STREAMLIT_KEY")
 
 st.set_page_config(page_title="스무고개 상담AI", page_icon="🧠")
 st.title("🧠 ChatGPT 스무고개 상담 AI")
